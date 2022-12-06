c4370a7943c318f41a6558c55f8dcef1420f21a1
Gaurav Dogra <gauru.dogra@gmail.com>
2022-12-06 22:15:16 +0000

    Added models

---
+++ TrackerLibrary/MatchupEntryModel.cs
@@ -0,0 +1,19 @@
+﻿namespace TrackerLibrary
+{
+    public class MatchupEntryModel
+    {
+        /// <summary>
+        /// Represent one team in the Matchup.
+        /// </summary>
+        public TeamModel TeamCompeting { get; set; }
+        /// <summary>
+        /// Represents the score for this particular team.
+        /// </summary>
+        public double Score { get; set; }
+        /// <summary>
+        /// Represents the matchup this team came from as a winner.
+        /// </summary>
+        public MatchupModel ParentMatchup { get; set; }
+
+    }
+}
---
+++ TrackerLibrary/MatchupModel.cs
@@ -0,0 +1,9 @@
+﻿namespace TrackerLibrary
+{
+    public class MatchupModel
+    {
+        public List<MatchupEntryModel> Entries { get; set; } = new();
+        public TeamModel Winner { get; set; }
+        public int Round { get; set; }
+    }
+}
